353998c12d8b3374032bd818f33b7ff5ae8f8162
oliviertheneworder <66775731+oliviertheneworder@users.noreply.github.com>
2024-05-14 13:53:23 +0200

    Update

--- klassworks.js
+++ klassworks.js
@@ -1,11 +1,28 @@
-// if #add-to-quote is clicked
 $('#add-to-quote').click(function() {
     var product = $('#product').text();
-    // for each .material-wrapper get the .material-heading text and the selected radio button's value.
-    // if .quote-stack is visible, append a new .quote-item containing a div.quote-details with .quote-product from the product variable and .quote-materials with the text and value from the radio buttons.
+    var materials = [];
+
+    // Collect all selected options
     $('.material-wrapper').each(function() {
         var material = $(this).find('.material-heading').text();
         var selected = $(this).find('input[type="radio"]:checked').val();
-        $('.quote-stack').append('<div class="quote-item"><div class="quote-details"><div class="quote-product">' + product + '</div><div class="quote-materials">' + material + ' - ' + selected + '</div></div><div class="input-number"><button class="button-increment minus">-</button><input class="input-field input-number" type="number" placeholder="1"><button class="button-increment plus">+</button></div><div class="quote-remove w-embed"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"></path></svg></div></div>');
+        if (selected) {
+            materials.push(material + ' - ' + selected);
+        }
     });
+
+    // Collect all selected options from checkboxes
+    $('.option-wrapper').each(function() {
+        var option = $(this).find('label').text();
+        var selected = $(this).find('input[type="checkbox"]:checked').length > 0 ? 'Yes' : 'No';
+        if (selected === 'Yes') {
+            materials.push(option);
+        }
+    });
+
+    // Join all selected options into a single string
+    var materialString = materials.join(' + ');
+
+    // Append a single quote-item to the .quote-stack
+    $('.quote-stack').append('<div class="quote-item"><div class="quote-details"><div class="quote-product">' + product + '</div><div class="quote-materials">' + materialString + '</div></div><div class="input-number"><button class="button-increment minus">-</button><input class="input-field input-number" type="number" value="1"><button class="button-increment plus">+</button></div><div class="quote-remove w-embed"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"></path></svg></div></div>');
 });
