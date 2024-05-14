5214f57256e9c1a643f4760913f7beab94ae1a82
oliviertheneworder <66775731+oliviertheneworder@users.noreply.github.com>
2024-05-14 13:54:16 +0200

    remove quote item

--- klassworks.js
+++ klassworks.js
@@ -26,3 +26,6 @@ $('#add-to-quote').click(function() {
     // Append a single quote-item to the .quote-stack
     $('.quote-stack').append('<div class="quote-item"><div class="quote-details"><div class="quote-product">' + product + '</div><div class="quote-materials">' + materialString + '</div></div><div class="input-number"><button class="button-increment minus">-</button><input class="input-field input-number" type="number" value="1"><button class="button-increment plus">+</button></div><div class="quote-remove w-embed"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"></path></svg></div></div>');
 });
+$('.quote-remove').click(function() {
+    $(this).closest('.quote-item').remove();
+}
