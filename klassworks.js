74c0b2ca0d3182c94ff8eae1b7a8ef45c4f3b8b9
oliviertheneworder <66775731+oliviertheneworder@users.noreply.github.com>
2024-06-12 12:30:26 +0200

    AltAir

--- klassworks.js
+++ klassworks.js
@@ -271,19 +271,30 @@ $(document).ready(function () {
         $('.material-heading.seat').text('Cushions');
     }
 
-    // if url contains /altair then duplicate the second visible .material-wrapper before this second .material-wrapper (childern of .material-stack), then change the text of .material-heading.seat to 'Back', then change all childeren ids by adding -2, then change all data attributes that have 'Material-Seat' or 'Material Seat' to 'Material-Seat' or 'Material Back'
+    // if url contains /altair then duplicate the second visible .material-wrapper before this second .material-wrapper (childern of .material-stack), then change the text of .material-heading.seat to 'Back', then change all childeren ids by adding -2, then change all custom attributes: name="Material-Seat" to name="Material-Back" and data-name="Material Seat" to data-name="Material Back", then all label for attribute add -2
     if (url.includes('/altair')) {
-        var secondMaterialWrapper = $('.material-wrapper:visible').eq(1).clone();
-        $('.material-wrapper:visible').eq(1).before(secondMaterialWrapper);
-        $('.material-wrapper:visible').eq(1).find('.material-heading.seat').text('Back');
-        $('.material-wrapper:visible').eq(1).find('[id]').each(function () {
+        var materialWrapper = $('.material-wrapper:visible').eq(1).clone();
+        materialWrapper.insertAfter($('.material-wrapper:visible').eq(1));
+        $('.material-wrapper:visible').eq(2).find('.material-heading.seat').text('Back');
+        $('.material-wrapper:visible').eq(2).find('*').each(function () {
             var id = $(this).attr('id');
-            $(this).attr('id', id + '-2');
-        });
-        $('.material-wrapper:visible').eq(1).find('[data-material-seat]').each(function () {
-            $(this).attr('data-material-back', $(this).attr('data-material-seat'));
-            $(this).removeAttr('data-material-seat');
-        });
+            if (id) {
+                $(this).attr('id', id + '-2');
+            }
+            var name = $(this).attr('name');
+            if (name) {
+                $(this).attr('name', name.replace('Seat', 'Back'));
+            }
+            var dataName = $(this).attr('data-name');
+            if (dataName) {
+                $(this).attr('data-name', dataName.replace('Seat', 'Back'));
+            }
+            var forAttr = $(this).attr('for');
+            if (forAttr) {
+                $(this).attr('for', forAttr + '-2');
+            }
+        }
+        );
     }
 
     // Function to update gallery thumb
